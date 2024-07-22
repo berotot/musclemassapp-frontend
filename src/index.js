c6c51e12ed4bdd40b3224a5d437111e2cf4e81ee
soerahmat69 <surahmate38@gmail.com>
2024-07-23 01:43:35 +0800

    re:credentials

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import route  from "./routes/route";
 import { AppContext } from "./store/AppContext";
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
