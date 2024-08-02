52cbbb1c46178ebed8512798562fdcc15831f26c
soerahmat69 <surahmatr38@gmail.com>
2024-08-03 00:47:54 +0800

    edit:loadskel

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,10 +5,11 @@ export const Profile = () => {
   return (
     <div className='p-4 h-screen'>
       <Navbar />
-      <main>
-        
+      <main className='p-4 h-screen'>
+        <div className=''>
+          <div className='h-[120px]  w-[120px] animate-pulse rounded-full   bg-slate-300 ' />
+        </div>
       </main>
-
     </div>
   )
 }
