fe6c2d67cbcc95be89cf9cdeace8addb7417799c
soerahmat69 <surahmatr38@gmail.com>
2024-08-03 01:41:49 +0800

    add:varvalue

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Navbar } from "../component/Navbar";
 import { MdEdit } from "react-icons/md";
+import { VerifyUser } from "../routes/route";
+import { FormModalAuthLogin } from "../component/FormModalAuth";
 
 export const Profile = () => {
+  const {userses, isError} = VerifyUser()
   return (
     <div className="p-4 h-screen">
       <Navbar />
+      {isError && <FormModalAuthLogin />}
       <main className="p-4 h-screen">
         <div className=" flex flex-col  items-center w-full  gap-3">
           <div className="h-[120px] mb-8  w-[120px] animate-pulse rounded-full   bg-slate-300 " />
@@ -14,16 +18,20 @@ export const Profile = () => {
               Username
             </p>
             <div className="flex items-center gap-2">
-              <div className="h-[15px] w-[96px] bg-slate-300 animate-pulse rounded-md"></div>
-              {/* <p className=" text-[15px] font-['poppins']">Anjayaani</p> */}
-              <MdEdit />
+              {userses[0].username ? 
+              <p className=" text-[15px] font-['poppins']">Anjayaani</p>
+              :  <div className="h-[15px] w-[96px] bg-slate-300 animate-pulse rounded-md"></div>
+            }
+            <MdEdit />
             </div>
           </div>
           <div className=" bg-white ring-1 ring-slate-300  py-4 rounded-md px-2 flex justify-between items-center w-full">
             <p className="text-[15px] font-semibold font-['poppins']">Email</p>
             <div className="flex items-center gap-2">
-              <div className="h-[15px] w-[70px] bg-slate-300 animate-pulse rounded-md"></div>
-              {/* <p className=" text-[15px] font-['poppins']">Anjayaani</p> */}
+              {userses[0].email ?
+              <p className=" text-[15px] font-['poppins']">Anjayaani</p>
+              :<div className="h-[15px] w-[70px] bg-slate-300 animate-pulse rounded-md"></div>
+              }
               <MdEdit />
             </div>
           </div>
@@ -32,9 +40,10 @@ export const Profile = () => {
               Password
             </p>
             <div className="flex items-center gap-2">
-              <div className="h-[15px] w-[100px] bg-slate-300 animate-pulse rounded-md"></div>
-              {/* <p className=" text-[15px] font-['poppins']">*******</p> */}
-              <MdEdit />
+              {userses[0].password?
+              <p className=" text-[15px] font-['poppins']">*******</p>
+              :<div className="h-[15px] w-[100px] bg-slate-300 animate-pulse rounded-md"></div>
+              }<MdEdit />
             </div>
           </div>
         </div>
