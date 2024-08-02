0c10b4603712d505eacf55d6ba5c9ce00c30fd4e
soerahmat69 <surahmatr38@gmail.com>
2024-08-02 22:58:29 +0800

    edit:sidebar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
+import { useSurvei } from "../store/AppContext";
 
 export const Navbar = () => {
   const [scroll, setScroll] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-const navigate = useNavigate()
+  const navigate = useNavigate();
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
@@ -24,12 +25,13 @@ const navigate = useNavigate()
   const handleToggle = () => {
     setMenuOpen(!menuOpen);
   };
+  const { verify } = useSurvei();
 
   const handleLogout = () => {
-    Cookies.remove('accessToken', { expires: 0 });
-    alert('Berhasil melakukan logout');
+    Cookies.remove("accessToken", { expires: 0 });
+    alert("Berhasil melakukan logout");
     // dispatch(reset());
-    navigate('/');
+    navigate("/");
   };
   return (
     <nav className="mb-[75px]">
@@ -46,9 +48,28 @@ const navigate = useNavigate()
           />
         </div>
         <ul className="sm:flex hidden gap-4">
-          <li onClick={()=>navigate('/')} className="cursor-pointer">Home</li>
-          <li onClick={()=>navigate('/leaderboard')} className="cursor-pointer">Scores</li>
-          <li onClick={()=>navigate('/aktivitas')} className="cursor-pointer">aktivitas</li>
+          <li onClick={() => navigate("/")} className="cursor-pointer">
+            Beranda
+          </li>
+          {verify.userses[0].username ? (
+            <li
+              onClick={() => navigate("/dashboard")}
+              className="cursor-pointer"
+            >
+              Dashboard
+            </li>
+          ) : (
+            ""
+          )}
+          <li
+            onClick={() => navigate("/leaderboard")}
+            className="cursor-pointer"
+          >
+            Skor
+          </li>
+          <li onClick={() => navigate("/aktivitas")} className="cursor-pointer">
+            Aktivitas
+          </li>
         </ul>
         <div className="sm:hidden relative">
           <div
@@ -62,17 +83,53 @@ const navigate = useNavigate()
             />
           </div>
           {menuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-40" onClick={handleToggle} />
-      )}
-            <div className={`fixed top-0 left-0 h-full bg-white shadow-md z-50 transition-transform transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-              <ul className="mt-16 space-y-2 p-4">
-                <li onClick={() => navigate('/')} className="cursor-pointer">Home</li>
-                <li onClick={() => navigate('/leaderboard')} className="cursor-pointer">Scores</li>
-                <li onClick={() => navigate('/aktivitas')} className="cursor-pointer">Aktivitas</li>
-                <li onClick={handleLogout} className="cursor-pointer">Logout</li>
-              </ul>
-            </div>
- 
+            <div
+              className="fixed inset-0 bg-black bg-opacity-50 z-40"
+              onClick={handleToggle}
+            />
+          )}
+          <div
+            className={` font-['poppins'] fixed top-0 left-0 h-full bg-white shadow-md z-50 transition-transform transform ${
+              menuOpen ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            <ul className="space-y-2 p-4">
+              <li onClick={() => navigate("/")} className="cursor-pointer">
+                Beranda
+              </li>
+
+              {verify.userses[0].username ? (
+                <>
+                  <li
+                  onClick={() => navigate("/dashboard")}
+                  className="cursor-pointer"
+                >
+                  Dashboard
+                </li>
+                <li
+                onClick={() => navigate("/aktivitas")}
+                className="cursor-pointer"
+              >
+                Aktivitas
+              </li>
+              <li onClick={handleLogout} className="cursor-pointer">
+                Logout
+              </li>
+              <li
+                onClick={() => navigate("/peringkat")}
+                className="cursor-pointer"
+              >
+                Peringkat
+              </li>
+                </>
+              
+              ) : (
+                ""
+              )}
+              
+             
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
